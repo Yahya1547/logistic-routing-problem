850a023ecf3e40f2a854faf80630f658c54b1bf1
Yahya <you@example.com>
2020-06-02 02:05:14 +0700

    draw map

--- src/draw.py
+++ src/draw.py
@@ -1,21 +1,39 @@
 from OpenGL.GL import *
 from OpenGL.GLUT import *
 from OpenGL.GLU import *
+from graf import *
+from math import *
 
-w,h= 500,500
-def square():
-    glBegin(GL_QUADS)
-    glVertex2f(100, 100)
-    glVertex2f(200, 100)
-    glVertex2f(200, 200)
-    glVertex2f(100, 200)
+dataNode = buildDataNode()
+
+def scale() :
+    for node in dataNode :
+        x = dataNode[node][0]
+        y = dataNode[node][1]
+
+        newX = x/10
+        newY = y/10
+        dataNode[node] = (newX, newY)
+
+scale()
+w,h= 1000,1000
+def map():
+    glBegin(GL_POINTS)
+    radius = 1.5
+    for node in dataNode :
+        posx, posy = dataNode[node][0], dataNode[node][1]
+
+        for i in range(360) :
+            cosine = radius * cos(i*pi/180) + posx
+            sine = radius * sin(i*pi/180) + posy
+            glVertex2f(cosine, sine)
     glEnd()
 
 def iterate():
-    glViewport(0, 0, 500, 500)
+    glViewport(0, 0, w, h)
     glMatrixMode(GL_PROJECTION)
     glLoadIdentity()
-    glOrtho(0.0, 500, 0.0, 500, 0.0, 1.0)
+    glOrtho(0.0, w, 0.0, h, 0.0, 1.0)
     glMatrixMode (GL_MODELVIEW)
     glLoadIdentity()
 
@@ -24,12 +42,12 @@ def showScreen():
     glLoadIdentity()
     iterate()
     glColor3f(1.0, 0.0, 3.0)
-    square()
+    map()
     glutSwapBuffers()
 
 glutInit()
 glutInitDisplayMode(GLUT_RGBA)
-glutInitWindowSize(500, 500)
+glutInitWindowSize(w, h)
 glutInitWindowPosition(0, 0)
 wind = glutCreateWindow("OpenGL Coding Practice")
 glutDisplayFunc(showScreen)
