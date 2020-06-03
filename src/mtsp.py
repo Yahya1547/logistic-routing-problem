6a1611e65b8bce93cbc41d384d44c05f8c5032f9
Yahya <you@example.com>
2020-06-04 01:58:26 +0700

    update milestone 2

--- src/mtsp.py
+++ src/mtsp.py
@@ -15,74 +15,79 @@ def solve() :
 
     start = int(input("Masukkan tempat perusahaan : "))
 
+    solutions = [-1 for i in range(m)]
+    solution_routes = [[] for i in range(m)]
+
     listNode = split(start, dest, m, dataNode)
     print(listNode)
-    indeks, subgraph, routes = buildSubgraph(dest, dataNode, dataEdge)
-    print(subgraph)
-    v = set(dest)
-    solution = [[] for i in range(m)]
-    model = Model()
+    for k in range(m) :
+        destination = listNode[k]
+        n = len(destination)
+        indeks, subgraph, routes = buildSubgraph(destination, dataNode, dataEdge)
+        print(subgraph)
+        print(indeks)
+        v = set(destination)
 
-    x = [[model.add_var(var_type=BINARY) for j in dest] for i in dest]
+        # building model
+        model = Model()
 
+        x = [[model.add_var(var_type=BINARY) for j in destination] for i in destination]
+        u = [model.add_var() for i in destination]
 
-    model.objective = minimize(xsum(subgraph[i][j]*x[i][j] for i in range(len(dest)) for j in range(len(dest))))
+        # adding constraints to the model
+        model.objective = minimize(xsum(subgraph[i][j]*x[i][j] for i in range(len(destination)) for j in range(len(destination))))
 
-    model += xsum(x[indeks[start]][indeks[j]] for j in v - {start} ) == m
+        model += xsum(x[indeks[start]][indeks[j]] for j in v - {start} ) == 1
 
-    model += xsum(x[indeks[j]][indeks[start]] for j in v - {start} ) == m
+        model += xsum(x[indeks[j]][indeks[start]] for j in v - {start} ) == 1
 
-    for i in dest :
-        model += xsum(x[indeks[i]][indeks[j]] for j in v - {i} ) == 1
+        for i in destination :
+            model += xsum(x[indeks[i]][indeks[j]] for j in v - {i} ) == 1
         
-
-    for i in dest :
-        model += xsum(x[indeks[j]][indeks[i]] for j in v - {i} ) == 1
+        for i in destination :
+            model += xsum(x[indeks[j]][indeks[i]] for j in v - {i} ) == 1
         
-    u = [model.add_var() for i in dest]
+        p = n + 1
 
-    p = n + 1
+        for (i,j) in product(v - {start}, v - {start}) :
+            if i != j :
+                model += u[indeks[i]] - u[indeks[j]] + p*x[indeks[i]][indeks[j]] <= p - 1
 
+        # model.max_mip_gap_abs = 0.05
+        status = model.optimize(max_seconds=30)
 
-    for (i,j) in product(v - {start}, v - {start}) :
-        if i != j :
-            model += u[indeks[i]] - u[indeks[j]] + p*x[indeks[i]][indeks[j]] <= p - 1
+        # find the solution
+        solutions[k] = model.objective_value
+        print("solution " + str(k+1) + " : " + str(model.objective_value))
 
-    model.max_mip_gap_abs = 0.05
-    # status = model.optimize(max_seconds=30,max_nodes = 100, max_solutions=1)
-    status = model.optimize(max_seconds=30)
-    print(status)
+        # setting up the route for the solution from mip solver
+        route = []
+        for i in range(len(destination)) :
+            if x[indeks[start]][i].x >= 1 :
+                print("ketemu " + str(indeks[i]))
 
-    print('route with total distances : ', end="")
-    if status == OptimizationStatus.OPTIMAL :
-        print(model.objective_value)
-    elif status == OptimizationStatus.FEASIBLE :
-        print(model.objective_value)
-    else :
-        print(model.objective_bound)
+    # route = []
 
-    route = []
-
-    for i in range(len(dest)) :
+    # for i in range(len(dest)) :
         
-        if x[indeks[start]][i].x >= 1 :
-            print("ketemu", end=" ")
-            route.append((start, dest[i], routes[indeks[start]][i]))
-            node = dest[i]
-            print(str(start) + " - ", end="")
-            while node != start :
-                print(str(node) + " - ", end="")
-                for j in range(len(dest)) :
-                    if x[indeks[node]][j].x >= 0.99 :
-                        route.append((node, dest[j], routes[indeks[node]][j]))
-                        node = dest[j]
-                        break
+    #     if x[indeks[start]][i].x >= 1 :
+    #         print("ketemu", end=" ")
+    #         route.append((start, dest[i], routes[indeks[start]][i]))
+    #         node = dest[i]
+    #         print(str(start) + " - ", end="")
+    #         while node != start :
+    #             print(str(node) + " - ", end="")
+    #             for j in range(len(dest)) :
+    #                 if x[indeks[node]][j].x >= 0.99 :
+    #                     route.append((node, dest[j], routes[indeks[node]][j]))
+    #                     node = dest[j]
+    #                     break
             
-            print(str(node))
-            # while node != start :
-            #     for j in range(len(dest)) :
-            #         if x[start][]
+    #         print(str(node))
+    #         # while node != start :
+    #         #     for j in range(len(dest)) :
+    #         #         if x[start][]
     
-    return route, dest
+    # return route, dest
 
 solve()
